59dc3081176ba17aab092560d7b5cf8ec78ed38d
fernando pedro <fernandopedro@fernandos-MacBook-Air.local>
2024-05-18 13:09:22 -0300

    :feature: cria componente HomeDefault

---
+++ src/views/HomeDefault.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="home">
+    <h1>Home</h1>
+    <nav>
+      <router-link to="/products">Manage Products</router-link>
+      <router-link to="/clients">Manage Clients</router-link>
+      <router-link to="/associate">Associate Products to Clients</router-link>
+    </nav>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeDefault'
+};
+</script>
+
+<style scoped>
+.home {
+  text-align: center;
+  margin-top: 50px;
+}
+
+nav {
+  margin: 20px 0;
+}
+
+nav a {
+  margin: 0 15px;
+  text-decoration: none;
+  color: #42b983;
+  font-weight: bold;
+}
+
+nav a:hover {
+  text-decoration: underline;
+}
+
+h1 {
+  color: #2c3e50;
+}
+</style>
