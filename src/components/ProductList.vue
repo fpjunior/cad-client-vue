df1b82eaf411298bb1bbc41328d8ccfda6ef1c7d
fernando pedro <fernandopedro@fernandos-MacBook-Air.local>
2024-05-18 13:06:48 -0300

    :feature: cria componente ProductList

---
+++ src/components/ProductList.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <h2>Product List</h2>
+    <ul>
+      <li v-for="product in products" :key="product.name">
+        {{ product.name }} - Active: {{ product.active ? 'Yes' : 'No' }}
+        <button @click="toggleActive(product)">Toggle Active</button>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ProductList',
+  props: {
+    products: {
+      type: Array,
+      required: true
+    }
+  },
+  methods: {
+    toggleActive(product) {
+      product.active = !product.active;
+    }
+  }
+};
+</script>
+
+<style scoped>
+h2 {
+  color: #2c3e50;
+}
+</style>
