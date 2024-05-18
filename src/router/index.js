b768f866fe648c2ca3cd6fd8820899be4a2814ed
fernando pedro <fernandopedro@fernandos-MacBook-Air.local>
2024-05-18 13:04:04 -0300

    :feature: renomeia nomes dos componentes para nao ocasionar problemas com as rotas

--- src/router/index.js
+++ src/router/index.js
@@ -1,22 +1,18 @@
-import Vue from 'vue';
-import VueRouter from 'vue-router';
-import Home from '../views/Home.vue';
-import Products from '../views/Products.vue';
-import Clients from '../views/Clients.vue';
-import Associate from '../views/Associate.vue';
-
-Vue.use(VueRouter);
+import { createRouter, createWebHistory } from 'vue-router';
+import HomeDefault from '../views/HomeDefault.vue';
+import ProductsDefault from '../views/ProductsDefault.vue';
+import ProductsClients from '../views/ProductsClients.vue';
+import ProductClientAssociation from '../views/ProductClientAssociation.vue';
 
 const routes = [
-  { path: '/', name: 'Home', component: Home },
-  { path: '/products', name: 'Products', component: Products },
-  { path: '/clients', name: 'Clients', component: Clients },
-  { path: '/associate', name: 'Associate', component: Associate },
+  { path: '/', name: 'HomeDefault', component: HomeDefault },
+  { path: '/products', name: 'ProductsDefault', component: ProductsDefault },
+  { path: '/clients', name: 'ProductsClients', component: ProductsClients },
+  { path: '/associate', name: 'ProductClientAssociation', component: ProductClientAssociation },
 ];
 
-const router = new VueRouter({
-  mode: 'history',
-  base: process.env.BASE_URL,
+const router = createRouter({
+  history: createWebHistory(process.env.BASE_URL),
   routes
 });
 
