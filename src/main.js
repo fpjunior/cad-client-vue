aaa5820eddd96e9a0e8bc9bcb7122d592b9ae203
fernando pedro <fernandopedro@fernandos-MacBook-Air.local>
2024-05-19 01:54:05 -0300

    :sparkles: configura o primevue no main.js

--- src/main.js
+++ src/main.js
@@ -1,11 +1,19 @@
 import { createApp } from 'vue';
 import App from './App.vue';
 import router from './router';
-import vuetify from './plugins/vuetify';  // Adicione esta linha
+
+import PrimeVue from 'primevue/config';
+import Button from 'primevue/button';
+import 'primevue/resources/themes/saga-blue/theme.css';  // Tema PrimeVue
+import 'primevue/resources/primevue.min.css';           // CSS PrimeVue
+import 'primeicons/primeicons.css';                     // Ícones PrimeIcons
 
 const app = createApp(App);
 
 app.use(router);
-app.use(vuetify);  // Adicione esta linha
+app.use(PrimeVue);
+
+// eslint-disable-next-line vue/multi-word-component-names
+app.component('Button', Button);
 
 app.mount('#app');
